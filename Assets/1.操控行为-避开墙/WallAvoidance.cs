4672c2b3121aac2d5d7faa746c2ce3cfdc34753d
liqingyun <liqingyun@putao.com>
2016-07-24 19:31:20 +0800

    第一次提交

---
+++ Assets/1.操控行为-避开墙/WallAvoidance.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// 避开墙行为
+/// </summary>
+public class WallAvoidance : MonoBehaviour {
+
+
+	/// <summary>
+	/// 速度
+	/// </summary>
+	public Vector3 velocity;
+
+	/// <summary>
+	/// 探针长度
+	/// </summary>
+	public float spinLength;
+
+	void Start()
+	{
+		spinLength = 4;
+	}
+
+	void Update()
+	{
+		Vector3 steeringForce = new Vector3 (0, 0, 0);
+		RaycastHit hitForward, hitLeft, hitRight;
+		Vector3 offset = new Vector3 (0, 1, 0);
+		Vector3 newPosition = transform.position + offset;
+		Debug.DrawLine (newPosition, newPosition + spinLength * transform.forward, Color.green);
+		Debug.DrawLine (newPosition, newPosition + spinLength * (transform.forward - transform.right), Color.green);
+		Debug.DrawLine (newPosition, newPosition + spinLength * (transform.forward + transform.right), Color.green);
+
+		if (Physics.Raycast (newPosition, transform.forward, out hitForward, spinLength, 1 << 8)) {
+			steeringForce += (spinLength - (newPosition - hitForward.point).magnitude) * hitForward.normal;
+		}
+			
+		if (Physics.Raycast (newPosition, transform.forward, out hitLeft, spinLength, 1 << 8)) {
+			steeringForce += (spinLength - (newPosition - hitLeft.point).magnitude) * hitForward.normal;
+		}
+
+		if (Physics.Raycast (newPosition, transform.forward, out hitRight, spinLength, 1 << 8)) {
+			steeringForce += (spinLength - (newPosition - hitRight.point).magnitude) * hitForward.normal;
+		}
+
+		Vector3 acc = steeringForce / 1;
+		velocity += acc * Time.deltaTime;
+
+		transform.position += velocity * Time.deltaTime;
+
+		if (velocity.magnitude > 0.01f) {
+			Vector3 newForward = Vector3.Slerp (transform.forward, velocity, Time.deltaTime);
+			newForward.y = 0;
+			transform.forward = newForward;
+		}
+	}
+}
---
+++ Assets/2.操控行为-靠近、远离/Seek.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seek : MonoBehaviour {
+
+	public float max_velocity;
+	public Vector3 velocity;
+	public Transform target;
+	// Use this for initialization
+	void Start()
+	{
+		max_velocity = 8;
+	}
+
+	// Update this called once per frame
+	void Update()
+	{
+		// disired_velocity = normalize(target.position - transform.position) * max_velocity
+		// steering = desired_velocity - currentVelocity
+		Vector3 desiredVelocity = (target.position - transform.position).normalized * max_velocity;
+		Vector3 steergingForce = desiredVelocity - velocity;
+
+		Vector3 acc = steergingForce / 1;
+		velocity += acc * Time.deltaTime;
+		transform.position += velocity * Time.deltaTime;
+
+		if (velocity.magnitude > 0.01f) {
+			Vector3 newForward = Vector3.Slerp (transform.forward, velocity, Time.deltaTime);
+
+			newForward.y = 0;
+			transform.forward = newForward;
+		}
+	}
+
+
+}
